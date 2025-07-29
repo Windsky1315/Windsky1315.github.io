cd003e53993a4764d08d028e95c3f11deefc26c9
Windsky1315 <night811090@gmail.com>
2025-07-30 00:06:40 +0800

    Update MyWebpage.js

--- Start/MyWebpage.js
+++ Start/MyWebpage.js
@@ -1,2 +1,2 @@
 "use strict";
-window.alert("Recommend using computer or ipad");
+window.alert("The website updates from time to time! To have a comfortable view, we recommend using computer or ipad");
