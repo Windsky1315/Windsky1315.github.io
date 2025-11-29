446e03b91891349a8815d5def6fb4561ee2031f7
Ariella1315 <ariel50161@gmail.com>
2025-11-30 01:31:52 +0800

    Update js file

--- Start/MyWebpage.js
+++ Start/MyWebpage.js
@@ -1,2 +1,17 @@
 "use strict";
 window.alert("The website updates from time to time! To have a comfortable view, we recommend using computer or ipad");
+
+const audio=document.getElementById('audio');
+const playButton=document.getElementById('playButton');
+
+playButton.addEventListener('click', ()=>{
+    if(audio.paused){
+        audio.play();
+        playButton.textContent='Pause';
+        playButton.classList.add('playing');
+    }else{
+        audio.pause();
+        playButton.textContent='Play';
+        playButton.classList.remove('playing');
+    }
+});
